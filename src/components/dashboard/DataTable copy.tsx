787776ce1ff463516ac31e9275ed2e0683bc64ed
Agustin Zalazar <agustin.zalazar@afforunity.com>
2025-01-03 18:25:20 -0300

    Fix error of props

--- src/components/dashboard/DataTable copy.tsx
+++ src/components/dashboard/DataTable copy.tsx
@@ -186,7 +186,7 @@ export function DataTable<TData, TValue>({
                     </TableBody>
                 </Table>
             </div>
-            <PaginationTable />
+            {/* <PaginationTable /> */}
             {/* <div className="flex items-center justify-end space-x-2 py-4">
                 <Button
                     variant="outline"
