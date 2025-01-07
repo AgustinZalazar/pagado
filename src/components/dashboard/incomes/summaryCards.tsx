71db7fc5984e1eaf8537d3c487f5d7071ac7432d
Agustin Zalazar <agustin.zalazar@afforunity.com>
2025-01-07 11:43:21 -0300

    Change summary cards design

--- src/components/dashboard/incomes/summaryCards.tsx
+++ src/components/dashboard/incomes/summaryCards.tsx
@@ -12,54 +12,76 @@ export function SummaryCards({ totalIncome, totalExpenses }: SummaryCardsProps) 
     const t = useTranslations('Dashboard.Incomes');
     return (
         <div className="flex flex-col md:flex-row gap-4">
-            <Card className="drop-shadow-md w-full md:w-[260px]">
+            <Card className="drop-shadow-md w-full md:w-[382px] h-fit">
                 <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">{t('card1')}</CardTitle>
-                    {/* <DollarSign className="h-4 w-4 text-muted-foreground" /> */}
-                    <TrendingUp className="h-4 w-4 text-[#00b743]" />
+                    <CardTitle className="text-xs font-medium text-muted-foreground">{t('card1')}</CardTitle>
                 </CardHeader>
-                <CardContent>
-                    <div className="text-2xl font-bold text-[#00b743]">${totalIncome.toFixed(2)}</div>
-                    <Separator className="mt-3" />
-                    <p className="mt-3 text-gray-400 text-sm">Mes anterior: <span className="font-semibold text-gray-500">$500</span></p>
+                <CardContent className="">
+                    <div className="flex gap-3 items-start">
+                        <div className="text-2xl font-bold text-black relative -top-1">${totalIncome.toFixed(2)}</div>
+                        <div className="flex bg-badge_light_green rounded-xl h-fit py-0.5 px-2">
+                            <TrendingUp className="h-3 w-3 text-badge_text_green" />
+                            <p className="text-xs text-badge_text_green">+3%</p>
+                        </div>
+                        <div className="ml-auto">
+                            <p className="text-xs text-muted-foreground">Mes Pasado</p>
+                            <p className="text-black text-sm font-semibold">$200</p>
+                        </div>
+                    </div>
                 </CardContent>
             </Card>
-            <Card className="drop-shadow-md w-full md:w-[260px]">
+            <Card className="drop-shadow-md w-full md:w-[382px] h-fit">
                 <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">{t('card2')}</CardTitle>
-                    {/* <DollarSign className="h-4 w-4 text-muted-foreground" /> */}
-                    <TrendingDown className="h-4 w-4 text-[#f95353]" />
+                    <CardTitle className="text-xs font-medium text-muted-foreground">{t('card2')}</CardTitle>
                 </CardHeader>
                 <CardContent>
-                    <div className="text-2xl font-bold text-[#f95353]">${totalExpenses.toFixed(2)}</div>
-                    <Separator className="mt-3" />
-                    <p className="mt-3 text-gray-400 text-sm">Mes anterior: <span className="font-semibold text-gray-500">$300</span></p>
+                    <div className="flex gap-3 items-start">
+                        <div className="text-2xl font-bold text-black relative -top-1">${totalExpenses.toFixed(2)}</div>
+                        <div className="flex bg-badge_light_red rounded-xl h-fit py-0.5 px-2">
+                            <TrendingDown className="h-3 w-3 text-badge_text_red" />
+                            <p className="text-xs text-badge_text_red">+3%</p>
+                        </div>
+                        <div className="ml-auto">
+                            <p className="text-xs text-muted-foreground">Mes Pasado</p>
+                            <p className="text-black text-sm font-semibold">$200</p>
+                        </div>
+                    </div>
                 </CardContent>
             </Card>
-            <Card className="drop-shadow-md w-full md:w-[260px]">
+            <Card className="drop-shadow-md w-full md:w-[400px] h-fit">
                 <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">{t('card3')}</CardTitle>
-                    {/* <DollarSign className="h-4 w-4 text-muted-foreground" /> */}
-                    <TrendingDown className="h-4 w-4 text-[#f95353]" />
+                    <CardTitle className="text-xs font-medium text-muted-foreground">{t('card3')}</CardTitle>
                 </CardHeader>
                 <CardContent>
-                    <div className="text-2xl font-bold text-[#f95353]">${totalExpenses.toFixed(2)}<br /><span className="text-sm"></span></div>
-                    <p className="text-sm font-bold text-black">Entretenimiento</p>
-                    <Separator className="mt-3" />
-                    <p className="mt-3 text-gray-400 text-sm">Mes anterior: <br /><span className="font-semibold text-gray-500">Comida&#10088;$250&#10089;</span></p>
+                    <div className="flex gap-3 items-start">
+                        <div className="text-2xl font-bold text-black relative -top-1">${totalExpenses.toFixed(2)}</div>
+                        <div className="flex bg-badge_light_red rounded-xl h-fit py-0.5 px-2">
+                            <TrendingDown className="h-3 w-3 text-badge_text_red" />
+                            <p className="text-xs text-badge_text_red">+3%</p>
+                        </div>
+                        <div className="ml-auto">
+                            <p className="text-xs text-muted-foreground">Mes Pasado</p>
+                            <p className="text-black text-xs font-semibold">$200 &#10088;Entretenimiento&#10089;</p>
+                        </div>
+                    </div>
                 </CardContent>
             </Card>
-            <Card className="drop-shadow-md w-full md:w-[260px]">
+            <Card className="drop-shadow-md w-full md:w-[400px] h-fit">
                 <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">{t('card4')}</CardTitle>
-                    {/* <DollarSign className="h-4 w-4 text-muted-foreground" /> */}
-                    <TrendingDown className="h-4 w-4 text-[#f95353]" />
+                    <CardTitle className="text-xs font-medium text-muted-foreground">{t('card3')}</CardTitle>
                 </CardHeader>
                 <CardContent>
-                    <div className="text-2xl font-bold text-[#f95353]">${totalExpenses.toFixed(2)}  <span className="text-[10px]">&#10088;Tarjeta de credito&#10089;</span></div>
-                    {/* <p className="text-sm font-bold text-black">Tarjeta de credito</p> */}
-                    <Separator className="mt-3" />
-                    <p className="mt-3 text-gray-400 text-sm">Mes anterior: <br /><span className="font-semibold text-gray-500">Tarjeta de debito&#10088;$300&#10089;</span></p>
+                    <div className="flex gap-3 items-start">
+                        <div className="text-2xl font-bold text-black relative -top-1">${totalExpenses.toFixed(2)}</div>
+                        <div className="flex bg-badge_light_red rounded-xl h-fit py-0.5 px-2">
+                            <TrendingDown className="h-3 w-3 text-badge_text_red" />
+                            <p className="text-xs text-badge_text_red">+3%</p>
+                        </div>
+                        <div className="ml-auto">
+                            <p className="text-xs text-muted-foreground">Mes Pasado</p>
+                            <p className="text-black text-xs font-semibold">$200 &#10088;Tarjeta de credito&#10089;</p>
+                        </div>
+                    </div>
                 </CardContent>
             </Card>
         </div>
