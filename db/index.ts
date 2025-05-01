c53bc7bab8f0e29d7ad4600af0f293767da88a27
Agustin Zalazar <agustin.zalazar@afforunity.com>
2025-04-30 23:11:19 -0300

    Add env for DB

--- db/index.ts
+++ db/index.ts
@@ -3,10 +3,15 @@ import mysql from "mysql2/promise";
 import { users } from "@/db/schema";
 
 const connection = await mysql.createConnection({
-    host: "*****",
-    user: "****",
-    password: "****",
-    database: "****",
+    // host: "localhost",
+    // user: "root",
+    // password: "admin",
+    // database: "pagado",
+    host: process.env.DATABASE_HOST,
+    port: +!process.env.DATABASE_PORT,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
 });
 
 export const db = drizzle(connection);
