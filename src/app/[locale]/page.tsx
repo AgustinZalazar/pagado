3c40b8dc4057dfb8bab5b486c9e807244ba0ddd4
Agustin Zalazar <agustin.zalazar@afforunity.com>
2025-08-11 12:35:37 -0300

    Change the way to verify google

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,13 +1,22 @@
 import { CurrencyProvider } from "@/components/landing/currency-context"
 import { LanguageProvider } from "@/components/landing/language-context"
 import LandingPage from "@/components/landing/landing-page"
+import Head from 'next/head'
 
 export default function Page() {
   return (
-
-    <CurrencyProvider>
-      <LandingPage />
-    </CurrencyProvider>
-    // </LanguageProvider>
+    <>
+      <Head>
+        <title>Pagado! - Gestiona tu dinero con Pagado!</title>
+        <meta name="description" content="Gestiona tu dinero con Pagado! - Gestiona tu dinero con Pagado!" />
+        <meta name="keywords" content="Pagado!, Gestiona tu dinero, Gestiona tu dinero con Pagado!" />
+        <meta name="author" content="Pagado!" />
+        <meta name="google-site-verification" content="_PGD5VRry7jI_vSJwRSOnjIc7uuxFrIrYZqvwRVch2k" />
+      </Head>
+      <CurrencyProvider>
+        <LandingPage />
+      </CurrencyProvider>
+      {/* // </LanguageProvider> */}
+    </>
   )
 }
